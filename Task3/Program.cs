fcd9e0bef78193bb9cbfa55c5d51bf5d166d3c74
Givi Chapichadze <chapichad@gmail.com>
2024-02-15 14:21:05 +0400

    changing the random number range

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,6 +1,13 @@
 ﻿using Org.BouncyCastle.Security;
 using Task3;
 
+
+if (args.Length != args.Distinct().Count())
+{
+    Console.WriteLine("Command line arguments contain duplicates! Try again!");
+    return;
+}
+
 if (args.Length < 3 || args.Length % 2 == 0)
 {
     Console.WriteLine("Incorrect number of arguments. Please provide an odd number >= 3.");
@@ -11,7 +18,7 @@ if (args.Length < 3 || args.Length % 2 == 0)
     Random rand = new Random();
     string key = KeyGeneration.GenerateKey();
 
-    int computerMove = rand.Next(1, 6); //1- rock, 2-spock, 3 - paper etc...
+    int computerMove = rand.Next(1, args.Length); //1- rock, 2-spock, 3 - paper etc...
     string hmac = GenerateHMAC.GetHMAC(computerMove.ToString(), key);
     Console.WriteLine($"HMAC: {hmac}");
 
